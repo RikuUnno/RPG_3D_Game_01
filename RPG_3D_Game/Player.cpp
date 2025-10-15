4009d71d7cdcc98333595e1a1303600126914faa
RikuUnno <riku.unno@gmail.com>
2025-10-15 14:19:34 +0900

    微変更

--- RPG_3D_Game/Player.cpp
+++ RPG_3D_Game/Player.cpp
@@ -13,24 +13,5 @@ Player::~Player()
 // 基本進行
 void Player::Update()
 {
-	if (m_isActive)
-	{
-		Activate();
-	}
-	else if (!m_isActive)
-	{
-		Deactivate();
-	}
-}
 
-// アクティブ時
-void Player::Activate()
-{
-	// collider.SetActiveCollider(m_isActive);	// 当たり判定のアクティブ化
-}
-
-// 非アクティブ時
-void Player::Deactivate()
-{
-	// collider.SetActiveCollider(!m_isActive); // 当たり判定の非アクティブ化
 }
--- RPG_3D_Game/Player.h
+++ RPG_3D_Game/Player.h
@@ -16,7 +16,5 @@ public:
 
 public: // 進行用
 	void Update();		// 基本進行の関数
-	void Activate();	// アクティブ時
-	void Deactivate();	// 非アクティブ時
 
 };
